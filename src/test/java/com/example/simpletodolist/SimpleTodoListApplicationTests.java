5ddddd0a81efa0855049b0fef44a8b8faed6dbc9
mohamdbarhoumi <barhoumimohamed@fsg.u-gabes.tn>
2025-09-19 10:34:35 +0100

    First commit

---
+++ src/test/java/com/example/simpletodolist/SimpleTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpletodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
